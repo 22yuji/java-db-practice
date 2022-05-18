e9d49265364758343090251fcb5fd89405527edc
yujiishadou <22ishado_yuuji@axiz.co.jp>
2022-05-18 12:27:14 +0900

    ソース簡略化

--- src/DbExam5.java
+++ src/DbExam5.java
@@ -1,5 +1,3 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.List;
 
 public class DbExam5 {
@@ -10,14 +8,23 @@ public class DbExam5 {
 	 */
 	
 	public static void main(String[] args) {
-		try {
+		
+		productDao = new ProductDao(DbUtil.getConnection());
+		
+		List<Product> list = productDao.findAll();
+        
+    	for(Product p : list) {
+    		System.out.println("product_id:" + p.getProductId() + ", product_name:" + p.getProductName() + ", price:" + p.getPrice());
+    	}
+		
+		/*try {
 	        // load JDBC Driver
 			Class.forName("org.postgresql.Driver");
 
 	        // try-with-resource
 	        try (Connection con = DriverManager.getConnection
 	            		("jdbc:postgresql://localhost:5432/dbconnection", "axizuser", "axiz");) {
-	        	productDao = new ProductDao(con);
+	        	productDao = new ProductDao(DbUtil.getConnection());
 	            List<Product> list = productDao.findAll();
 	                
 	        	for(Product p : list) {
@@ -26,6 +33,6 @@ public class DbExam5 {
 	        }
 	    } catch (Exception e) {
 	            e.printStackTrace();
-	    }
+	    }*/
     }
 }
